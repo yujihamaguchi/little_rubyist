4e406ab044186fa1287b8471025414bd9b3809f4
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-08-09 07:28:01 +0900

    WIP

--- logic.rb
+++ logic.rb
@@ -1,6 +1,22 @@
-def my_zip(*colls)
-  return nil if colls.empty?
-  colls.first.zip(*colls[1..]).reject { |tuple| tuple.include?(nil) }
+# Q001: haskell の zip と同様の機能の関数 my-zip を書け （パラメータの数は可変であること）
+# zip :: [a] -> [b] -> [(a, b)]
+# not using recursion
+class Array
+  def my_zip(*lists)
+    return self if lists.empty?
+    # self.zip(*lists).reject { |tuple| tuple.include?(nil) }
+    lists.unshift(self).transpose
+  end
+end
+
+# def my_zip(*colls)
+#   return nil if colls.empty?
+#   colls.first.zip(*colls[1..]).reject { |tuple| tuple.include?(nil) }
+# end
+
+def foo(arg, *args)
+  puts "arg > #{arg}"
+  puts "args > #{args}"
 end
 
 def fizzbuzz(number)
--- test_logic.rb
+++ test_logic.rb
@@ -2,19 +2,15 @@ require 'minitest/autorun'
 require_relative 'logic'
 
 class TestLogic < Minitest::Test
-  # Q001: haskell の zip と同様の機能の関数 my-zip を書け （パラメータの数は可変であること）
-  # zip :: [a] -> [b] -> [(a, b)]
-  # not using recursion
   def test_my_zip
-    assert_equal [[1, 'a', 'x'], [2, 'b', 'y']], my_zip([1, 2], ['a', 'b'], ['x', 'y'])
-    assert_equal [[1, 'x']], my_zip([1, 2], ['x'])
-    assert_equal [[1, 'x']], my_zip([1], ['x', 'y'])
-    assert_equal nil, my_zip()
-    assert_equal [], my_zip([])
-    assert_equal [], my_zip([], [1])
-    assert_equal [], my_zip([1], [])
+    assert_equal [[1, 'a', 'x'], [2, 'b', 'y']], [1, 2].my_zip(['a', 'b'], ['x', 'y'])
+    # assert_equal [[1, 'x']], [1, 2].my_zip(['x'])
+    # assert_equal [[1, 'x']], [1].my_zip(['x', 'y'])
+    # assert_equal [], [].my_zip([])
+    # assert_equal [], [].my_zip([1])
+    # assert_equal [], [1].my_zip([])
   end
-    
+
   def test_fizzbuzz
     assert_equal '1', fizzbuzz(1)
     assert_equal '2', fizzbuzz(2)
