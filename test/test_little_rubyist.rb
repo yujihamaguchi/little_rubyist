555dc67ac065815018ff02bcd34a14d6f3632043
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-08-12 07:34:54 +0900

    WIP

--- test/test_little_rubyist.rb
+++ test/test_little_rubyist.rb
@@ -9,11 +9,11 @@ class TestLittleRubyist < Minitest::Test
 
   def test_my_zip
     assert_equal [[1, 'a', 'x'], [2, 'b', 'y']], [1, 2].my_zip(['a', 'b'], ['x', 'y'])
-    # assert_equal [[1, 'x']], [1, 2].my_zip(['x'])
-    # assert_equal [[1, 'x']], [1].my_zip(['x', 'y'])
-    # assert_equal [], [].my_zip([])
-    # assert_equal [], [].my_zip([1])
-    # assert_equal [], [1].my_zip([])
+    assert_equal [[1, 'x']], [1, 2].my_zip(['x'])
+    assert_equal [[1, 'x']], [1].my_zip(['x', 'y'])
+    assert_equal [], [].my_zip([])
+    assert_equal [], [].my_zip([1])
+    assert_equal [], [1].my_zip([])
   end
 
 end
