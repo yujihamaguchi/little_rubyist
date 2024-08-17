275c18185aea9b11f960c9913d80f07abc33f8c9
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-08-18 06:43:20 +0900

    Q001 is done

--- lib/little_rubyist.rb
+++ lib/little_rubyist.rb
@@ -6,22 +6,18 @@ class Array
   # Q001: haskell の zip と同様の機能の関数 my-zip を書け （パラメータの数は可変であること）
   # zip :: [a] -> [b] -> [(a, b)]
   # not using recursion
-  #   def my_zip(*lists)
-  #   return self if lists.empty?
-  #
-  #   lists.unshift(self)
-  #   length = lists.min_by(&:length).length
-  #   lists.map { |list| list.take(length) }.transpose
-  # end
-  # using recursion
   def my_zip(*lists)
-    lists.unshift(self)
-    return [] if lists.empty?
-    return [] if lists.any? { |list| list.empty? }
-    [lists.map{ |list| list.first }] + my_zip(*lists.map{|list| list.drop(1) })
-  end
-end
+    return self if lists.empty?
 
-module LittleRubyist
-  class Error < StandardError; end
+    lists.unshift(self)
+    length = lists.min_by(&:length).length
+    lists.map { |list| list.take(length) }.transpose
+  end
+  # using recursion
+  # def my_zip(*lists)
+  #   return [] if self.empty? || lists.any?(&:empty?)
+  #
+  #   self.drop(1).my_zip(*lists.map { |list| list.drop(1) })
+  #       .prepend(lists.map(&:first).prepend(self.first))
+  # end
 end
