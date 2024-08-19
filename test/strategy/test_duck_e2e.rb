500f856abe2042b8a313e8d1b7d9e92f4db710d7
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-08-19 18:02:41 +0900

    strategy is done

--- test/strategy/test_duck_e2e.rb
+++ test/strategy/test_duck_e2e.rb
@@ -1,13 +1,17 @@
-require "minitest/autorun"
-require_relative '../../lib/strategy/duck'
-require_relative '../../lib/strategy/fly_with_wings'
+# frozen_string_literal: true
 
-class DuckTestE2e < Minitest::Test
+require "minitest/autorun"
+require_relative "../../lib/strategy/duck"
+require_relative "../../lib/strategy/fly_with_wings"
+
+class DuckE2eTest < Minitest::Test
   def test_fly
     # Arrange
     duck = Duck.new(fly_behavior: FlyWithWings.new)
+
     # Act
     actual = duck.fly
+
     # Assert
     assert_equal "Flying with wings...", actual
   end
