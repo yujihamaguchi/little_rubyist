c6556f16375d0be9a15e283ce5b36c89bd7845a3
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-08-19 15:46:41 +0900

    WIP

--- test/strategy/test_duck_e2e.rb
+++ test/strategy/test_duck_e2e.rb
@@ -1,7 +1,14 @@
 require "minitest/autorun"
+require_relative '../../lib/strategy/duck'
+require_relative '../../lib/strategy/fly_with_wings'
 
 class DuckTestE2e < Minitest::Test
   def test_fly
-    assert_equal 1, 1
+    # Arrange
+    duck = Duck.new(fly_behavior: FlyWithWings.new)
+    # Act
+    actual = duck.fly
+    # Assert
+    assert_equal "Flying with wings...", actual
   end
 end
