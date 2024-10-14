076c9e1bf264ea1ba325ae9e6e8626dcf4241213
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-10-15 07:20:31 +0900

    WIP

--- lib/adaptor/customer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Customer
-  attr_accessor :supplier
-
-  def arrival_date
-    supplier.delivery_date
-  end
-end
--- lib/adaptor/supplier.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Supplier
-  attr_accessor :delivery_date
-end
