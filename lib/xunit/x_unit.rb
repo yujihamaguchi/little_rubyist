a05f842341e69b853dcb56ed3fd76f5fe5553fb5
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-11-26 13:13:32 +0900

    Ch.19 is done

--- lib/xunit/x_unit.rb
+++ lib/xunit/x_unit.rb
@@ -5,17 +5,20 @@ class TestCase
     @name = name
   end
 
+  def set_up; end
+
   def run
+    self.set_up
     self.send(@name)
   end
 end
 
 class WasRun < TestCase
-  attr_reader :was_run
+  attr_reader :was_run, :was_set_up
 
-  def initialize(name)
+  def set_up
     @was_run = nil
-    super(name)
+    @was_set_up = 1
   end
 
   def test_method
@@ -24,13 +27,20 @@ class WasRun < TestCase
 end
 
 class TestCaseTest < TestCase
-  def test_running
-    test = WasRun.new("test_method")
-    raise if test.was_run
+  def set_up
+    @test = WasRun.new("test_method")
+  end
 
-    test.run
-    raise unless test.was_run
+  def test_running
+    @test.run
+    raise unless @test.was_run
+  end
+
+  def test_set_up
+    @test.run
+    raise unless @test.was_set_up
   end
 end
 
 TestCaseTest.new("test_running").run
+TestCaseTest.new("test_set_up").run
