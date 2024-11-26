dc8b87f06ed2c0915aae72ce2e4149d556469415
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2024-11-26 14:10:41 +0900

    Ch.23 is done

--- lib/xunit/x_unit.rb
+++ lib/xunit/x_unit.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
+class TestSuite
+  def initialize
+    @tests = []
+  end
+
+  def add(test)
+    @tests << test
+  end
+
+  def run(result)
+    @tests.each do |test|
+      test.run(result)
+    end
+  end
+end
+
 class TestResult
   def initialize
     @run_count = 0
@@ -28,8 +44,7 @@ class TestCase
 
   def tear_down; end
 
-  def run
-    result = TestResult.new
+  def run(result)
     result.test_started
     self.set_up
     begin
@@ -38,7 +53,6 @@ class TestCase
       result.test_failed
     end
     self.tear_down
-    result
   end
 end
 
@@ -63,33 +77,49 @@ class WasRun < TestCase
 end
 
 class TestCaseTest < TestCase
+  def set_up
+    @result = TestResult.new
+  end
+
   def test_template_method
     @test = WasRun.new("test_method")
-    @test.run
+    @test.run(@result)
     raise unless @test.log == "setUp testMethod tearDown "
   end
 
   def test_result
-    test = WasRun.new("test_method")
-    result = test.run
-    raise unless result.summary == "1 run, 0 failed"
+    @test = WasRun.new("test_method")
+    @test.run(@result)
+    raise unless @result.summary == "1 run, 0 failed"
   end
 
   def test_failed_result
-    test = WasRun.new("test_broken_method")
-    result = test.run
-    raise unless result.summary == "1 run, 1 failed"
+    @test = WasRun.new("test_broken_method")
+    @test.run(@result)
+    raise unless @result.summary == "1 run, 1 failed"
   end
 
   def test_failed_result_formatting
-    result = TestResult.new
-    result.test_started
-    result.test_failed
-    raise unless result.summary == "1 run, 1 failed"
+    @result.test_started
+    @result.test_failed
+    raise unless @result.summary == "1 run, 1 failed"
+  end
+
+  def test_suite
+    suite = TestSuite.new
+    suite.add(WasRun.new("test_method"))
+    suite.add(WasRun.new("test_broken_method"))
+    suite.run(@result)
+    raise unless @result.summary == "2 run, 1 failed"
   end
 end
 
-TestCaseTest.new("test_template_method").run
-TestCaseTest.new("test_result").run
-TestCaseTest.new("test_failed_result").run
-TestCaseTest.new("test_failed_result_formatting").run
+suite = TestSuite.new
+suite.add(TestCaseTest.new("test_template_method"))
+suite.add(TestCaseTest.new("test_result"))
+suite.add(TestCaseTest.new("test_failed_result"))
+suite.add(TestCaseTest.new("test_failed_result_formatting"))
+suite.add(TestCaseTest.new("test_suite"))
+result = TestResult.new
+suite.run(result)
+p result.summary
