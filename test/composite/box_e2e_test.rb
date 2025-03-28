21020e26ad60f6075c3294ba1a3f23bae31d5fd3
Yuji Hamaguchi <hamaguchi.u2@gmail.com>
2025-03-28 16:08:08 +0900

    tweak

--- test/composite/box_e2e_test.rb
+++ test/composite/box_e2e_test.rb
@@ -6,22 +6,23 @@ require_relative "../../lib/composite/product"
 
 # 部分が組み合わさった構成物に対してメッセージを送り、構成物総体としてのレスポンスを得たい
 class BoxE2eTest < Minitest::Test
+  def setup
+    @box = Box.new
+    @product1 = Product.new(price: 50)
+    @product2 = Product.new(price: 30)
+    @small_box = Box.new
+    @small_product = Product.new(price: 20)
+  end
+
   def test_price
     # Arrange
-    box = Box.new
-    product1 = Product.new(price: 50)
-    product2 = Product.new(price: 30)
-    box.add(product1)
-    box.add(product2)
-
-    small_box = Box.new
-    small_product = Product.new(price: 20)
-    small_box.add(small_product)
-
-    box.add(small_box)
+    @box.add(@product1)
+    @box.add(@product2)
+    @small_box.add(@small_product)
+    @box.add(@small_box)
 
     # Act
-    total_price = box.price
+    total_price = @box.price
 
     # Assert
     assert_equal 100, total_price
